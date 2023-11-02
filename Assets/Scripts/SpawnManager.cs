03fa1ecd8b99a7d5594bfc7d0d2c9511e2b96558
ZombieIce08 <146860778+ZombieIce08@users.noreply.github.com>
2023-11-02 10:10:26 -0700

    finished 4.4
    I finished 4.4
    
    I'm getting faster that make me happy.
    This is ZombieIce08

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,21 +5,38 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerUpPrefab;
     public float spawnRange = 9.0f;
-    // Start is called before the first frame update
+    public int enemyCount;
+    public int waveNumber = 1;
+
+ // Start is called before the first frame update
     void Start()
     {
-        
-
-
-
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
 
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+        }
+
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
     private Vector3 GenerateSpawnPosition()
     {
